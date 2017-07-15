2d90ff72ca8eaa378ba20c394f2f19fc5350ca79
xiaohuihui <704866169@qq.com>
2017-07-15 23:19:03 +0800

    修改

--- constant/url_website/url_data.py
+++ constant/url_website/url_data.py
@@ -8,7 +8,7 @@
 
 class url_content(object):
     #主线链接
-    url_thread = 'http://seller.t-lianni.com/user/login?f=/'
+    url_thread = 'http://---/user/login?f=/'
     #登录链接
     url_login = url_thread + ''
 
