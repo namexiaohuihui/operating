6c5ca8d0af1f4f9b474272fd572d24b45ddec8b9
xiaohuihui <704866169@qq.com>
2017-12-13 22:08:01 +0800

    又没弄东西

--- practical/utils/OpenpyxlExcel.py
+++ practical/utils/OpenpyxlExcel.py
@@ -36,7 +36,7 @@ class READEXCEL:
         else:
             raise FileNotFoundError('文件不存在！')
 
-    def data(self, sheet):
+    def data(self, sheet):#　此处不严谨：如果输入的字符串都为数字那么就会出错
 
         # 创建需要操作的文档
         self.workbook = load_workbook(filename=self.excel)  # 打开文档
@@ -47,7 +47,7 @@ class READEXCEL:
 
         # 如果是文字
         elif type(sheet) in [str]:
-            self.sheetbook = self.workbook[self.workbook.sheetnames[0]]
+            self.sheetbook = self.workbook[self.workbook.sheetnames[sheet]]
 
         # 长度过长时提示
         elif sheet >= len(self.workbook.sheetnames):
@@ -62,9 +62,16 @@ class READEXCEL:
         # 先判断需要寻找的cell位置。如果为空或者其他类型的就提示
         if type(range) in [int, str]:
             content = self.sheetbook[range]
+            # 打印指定的内容:ws['A4']返回的是一个cell，通过value来获取值
+            # content = self.sheetbook[range].value
             return content
         else:
             print('1级错误')
+
+    #   指定行和列来读取数据信息
+    def position_sheet_value(self,min_row=1, max_row=1, max_col=1 ):
+        content = self.sheetbook.iter_rows(min_row=min_row, max_row=max_row, max_col=max_col)
+        return content
 
     #   工作薄的名称
     def get_sheet_title(self):
@@ -73,6 +80,83 @@ class READEXCEL:
     #   返回某个列的标题位置名称
     def get_column_letter(self,Number = 1):
         return get_column_letter(Number)
+
+    #   返回当前工作薄的复制体对象
+    def replica_worksheet(self):
+        copy_sheet = self.workbook.copy_worksheet(self.sheetbook)
+        return copy_sheet
+
+    #   为真时，以行为一体，每行的数据信息
+    #   为假时，以列为一体，每列的数据信息
+    def total_row_columns(self,total = True):
+        if total:
+            content = tuple(read_excel.sheetbook.rows)# 单行中，列的长度
+        else:
+            content = tuple(read_excel.sheetbook.columns)# 单列中，行的长度
+        return content;
+
+    def attribute_template(self,emplate = None):
+        # 将现有的表进行复制并保存为模板。。并后缀名为xltx，如果为xls和xlsx在打开的时候出现问题
+        if type(emplate) in [str] :#  判断输入的内容是否为字符串
+
+            genericpath = os.path.splitext(emplate)[1] #    切割文件后缀名
+
+            if genericpath =='.xltx':
+
+                attribute = os.path.split(emplate)[1] # 切割最后一个文件的名字
+                self.workbook.template = True   # 属性设置
+                self.workbook.save(attribute)   # 保存后缀名为xltx的文件
+                return attribute;   # 返回文件名
+
+            elif genericpath == '':
+                attribute = os.path.split(emplate)[1] + '.xltx'# 切割最后一个文件的名字
+                self.workbook.template = True# 属性设置
+                self.workbook.save(attribute)# 保存后缀名为xltx的文件
+                return attribute; # 返回文件名
+
+            elif genericpath in ['xls','xlsx','txt']:
+                print('Files that do not support suffixes such as XLS xlsx text')
+
+            else:
+                print('The input file suffix name does not conform.')
+
+        elif emplate == None:
+            attribute = 'copy_' + self.excel + '.xltx'  # 设置文件的名字
+            self.workbook.template = True
+            self.workbook.save('copy_' + self.excel + '.xltx' )
+            return attribute
+
+        else:
+            print('你丫的文件输入有误')
+
+    def attribute_document(self,document):
+        # 将现有的模板还原成文档或直接将现有的wb另存为。
+        # 保存为xls文件打开的时候会提示错误
+        if type(document) in [str] :#  判断输入的内容是否为字符串
+
+            genericpath = os.path.splitext(document)[1] #    切割文件后缀名
+
+        if os.path.exists(document): # 判断文件是否存在
+            genericpath = os.path.splitext(document)[1]  #    切割文件后缀名
+            if genericpath == '.xltx' :
+                attribute = os.path.split(emplate)[1]  # 切割最后一个文件的名字
+                self.workbook.template = True  # 属性设置
+                self.workbook.save(attribute)  # 保存后缀名为xltx的文件
+
+                self.workbook.template = False
+                self.workbook.save('nihaoma.xlsx')
+                return attribute;  # 返回文件名
+
+            elif genericpath == '':
+                attribute = os.path.split(emplate)[1]  # 切割最后一个文件的名字
+                self.workbook.template = True  # 属性设置
+                self.workbook.save(attribute)  # 保存后缀名为xltx的文件
+                return attribute;  # 返回文件名
+
+
+    def qisahife(self):
+        #　打印工作薄名称
+        print(wb.sheetnames)
 
 
 """
@@ -88,6 +172,6 @@ class WRITEEXCEL:
 if __name__ == '__main__':
     read_excel = READEXCEL('empty_book.xlsx')
     # print(read_excel.get_sheet_value('1'))
-    print(read_excel.get_column_letter(1))
-    for kk in read_excel.get_sheet_value(1):
-        print(kk.value)
+    print(os.path.exists('E:'))
+
+
