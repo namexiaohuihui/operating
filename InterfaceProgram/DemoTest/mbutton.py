55f7e8a8e0c576e03e43a713c4215a7600457259
xiaohuihui <704866169@qq.com>
2018-06-20 12:04:28 -0300

    下拉框

---
+++ InterfaceProgram/DemoTest/mbutton.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*- 
+"""
+@__author__ :DingDong
+@file: mbutton.py
+@time: 2018/6/20 10:32
+@Entry Name:operating
+"""
+from tkinter import *
+root = Tk()
+mbutton = Menubutton(root,text = 'Food')
+pickl = Menu(mbutton)
+mbutton.config(menu = pickl)
+
+pickl.add_command(label = 'spam',command = root.quit)
+pickl.add_command(label = 'eggs',command = root.quit)
+pickl.add_command(label = 'bacon',command = root.quit)
+
+mbutton.pack()
+mbutton.config(bg = 'white',bd = 4,relief = RAISED)
+root.mainloop()
