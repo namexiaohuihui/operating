7028109757dc6db334f4812e43d50e6f8644fc4e
dingdong <704866169@qq.com>
2018-09-17 09:00:17 +0800

    清理一些不需要的东西

--- CenterBackground/SystemSetup/NavigationDirectory/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-__author__ = 'DingDong'
-"""
-@file: __init__.py.py
-@time: 2018/5/24 15:55
-"""
--- CenterBackground/SystemSetup/StatusTask/taskNames.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-__author__ = 'DingDong'
-"""
-@file: taskNames.py
-@time: 2018/5/25 14:13
-"""
-class TaskNames(object):
-    pass
